c7d8a23f37c211794d9d2017e251dcf29a279266
vjenditahimaj <56451456+vjenditahimaj@users.noreply.github.com>
2020-07-16 17:08:08 +0200

    Update WebDriverPractice.java

--- BasicNavigations/target/test-classes/WebDriverPractice.java
+++ BasicNavigations/target/test-classes/WebDriverPractice.java
@@ -44,6 +44,41 @@ public class WebDriverPractice {
         driver.findElement(By.name("lastname")).sendKeys("q");
         Assert.assertTrue(driver.findElement(By.xpath("(//small[@class='help-block'])[2]")).isDisplayed());
     }
+        @Test
+    public static void TestCase4() {
+        driver.get("https://practice-cybertekschool.herokuapp.com");
+        driver.findElement(By.xpath("(//li[@class='list-group-item']//a[@href='/registration_form'])")).click();
+
+        driver.findElement(By.name("lastname")).sendKeys("q");
+        Assert.assertTrue(driver.findElement(By.xpath("(//small[@class='help-block'])[6]")).isDisplayed());
+    }
+    @Test
+    public static void testCase5(){
+        driver.get("https://practice-cybertekschool.herokuapp.com/");
+        driver.findElement(By.xpath("(//li[@class='list-group-item']//a[@href='/registration_form'])")).click();
+
+
+        driver.findElement(By.name("firstname")).sendKeys("Vjendita");
+        driver.findElement(By.name("lastname")).sendKeys("Himaj");
+        driver.findElement(By.name("username")).sendKeys("Vjenditahimaj");
+        driver.findElement(By.name("email")).sendKeys("vjendita@cybertekschool.com");
+        driver.findElement(By.name("password")).sendKeys("Vjendita123");
+        driver.findElement(By.name("phone")).sendKeys("123-456-7855");
+        driver.findElement(By.xpath("//input[@value='female']")).click();
+        driver.findElement(By.name("birthday")).sendKeys("11/05/1995");
+        Select department = new Select(driver.findElement(By.name("department")));
+        department.selectByValue("DE");
+        Select jobTitle = new Select(driver.findElement(By.name("job_title")));
+        jobTitle.selectByVisibleText("SDET");
+        driver.findElement(By.id("inlineCheckbox1")).click();
+
+        //Click Sign up
+        driver.findElement(By.id("wooden_spoon")).click();
+
+        //Verify that following success message is displayed: “You've successfully completed registration!”
+        String success = driver.findElement(By.xpath("//div[@class='alert alert-success']//p")).getText();
+        Assert.assertEquals(success,"You've successfully completed registration!");
+    }
 
     @AfterMethod
     public static void closeDriver(){
